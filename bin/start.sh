989610303fc80518235914bc682941ff0d07fe05
Stéphane Cottin <stephane.cottin@vixns.com>
2017-01-23 00:57:36 +0100

    Revert "remove references to PORT0 and PORT1" Compute LIBPROCESS_IP from eth0 Use PORT1 as LIBPROCESS_PORT
    This reverts commit 19a0bc85c9307f683284414c97cd918cc4523443.

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+LIBPROCESS_IP="$(ifconfig eth0 | egrep -o 'addr:([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}' | egrep -o '[[:digit:]].*')" \
+LIBPROCESS_PORT="${PORT1}" \
 java $JVM_OPTS -jar /chronos/chronos.jar $@
