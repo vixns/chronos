0fb490394d3d29367db7b2a0111de9332aff88ff
Stéphane Cottin <stephane.cottin@vixns.com>
2017-01-23 01:15:43 +0100

    set ifconfig and egrep full paths

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-LIBPROCESS_IP="$(ifconfig eth0 | egrep -o 'addr:([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}' | egrep -o '[[:digit:]].*')" \
+LIBPROCESS_IP="$(/sbin/ifconfig eth0 | /bin/egrep -o 'addr:([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}' | /bin/egrep -o '[[:digit:]].*')" \
 LIBPROCESS_PORT="${PORT1}" \
 java $JVM_OPTS -jar /chronos/chronos.jar $@
