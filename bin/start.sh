9f4fbe6bc5a9f69d67de12eaf61cec18a0b09907
Stéphane Cottin <stephane.cottin@vixns.com>
2017-01-23 01:28:48 +0100

    use ip, not ifconfig

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-LIBPROCESS_IP="$(/sbin/ifconfig eth0 | /bin/egrep -o 'addr:([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}' | /bin/egrep -o '[[:digit:]].*')" \
+LIBPROCESS_IP="$(ip -o -4 addr show dev eth0 | sed 's/.* inet \([^/]*\).*/\1/')" \
 LIBPROCESS_PORT="${PORT1}" \
 exec java $JVM_OPTS -jar /chronos/chronos.jar $@
