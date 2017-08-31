b55ff3458b8de7ef27d1958b23147fe3ea0f3a47
Stéphane Cottin <stephane.cottin@vixns.com>
2017-08-31 10:51:35 +0200

    fix chronos path

--- bin/start.sh
+++ bin/start.sh
@@ -4,4 +4,4 @@ LIBPROCESS_IP="$(ip -o -4 addr show dev eth0 | sed 's/.* inet \([^/]*\).*/\1/')"
 LIBPROCESS_PORT="${PORT1}" \
 MESOS_NATIVE_LIBRARY=/usr/local/lib/libmesos.so \
 MESOS_NATIVE_JAVA_LIBRARY=/usr/local/lib/libmesos.so \
-exec java $JVM_OPTS -jar /chronos/chronos.jar $@
+exec java $JVM_OPTS -jar /chronos.jar $@
